4b5436b9b6b5eec89104bec0871e20fb0cbe4f14
Niranj Rajesh <niranjrajesh7007@gmail.com>
2021-06-26 10:45:43 +0530

    pages completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router,Switch,Route} from  'react-router-dom';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact />
+        <Route path="/" exact  component={Home}/>
+        <Route path="/services" exact  component={Services}/>
+        <Route path="/products" exact  component={Products}/>
+
       </Switch>
     </Router>
      
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import HeroSection from '../components/HeroSection';
+import Cards from '../components/Cards';
+
+
+ const Home = (params) => {
+    return(
+        <>
+        <HeroSection/>
+        <Cards/>
+        </>
+    )
+    
+}
+export default Home;
